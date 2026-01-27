96a4f32b02bf2c228c14ccc9634b90b571f63fa5
Benraouane Soufiane <benrawane2012@gmail.com>
2026-01-27 17:26:13 +0100

    fix: map attn.c_proj into attention.wo

--- models/tt_transformers/tt/load_checkpoints.py
+++ models/tt_transformers/tt/load_checkpoints.py
@@ -651,6 +651,7 @@ def map_hf_to_meta_keys(loaded_weights):
     
     if is_phi1:
         replacements.extend([
+            ("attn.c_proj", "attention.wo"),
             ("fc1", "w1"),
             ("fc2", "w2"),
         ])
